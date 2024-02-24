19e3dc17335855d0e9f64cb4087619ee2b34e2de
PCuser63 <salnikov228@list.ru>
2024-02-24 11:20:34 +0400

    8

---
+++ py16.py
@@ -0,0 +1,7 @@
+x = int(input())
+y = int(input())
+if x>=2 and x<=3 and y>= -5 and y<= -2:
+    print("no yes")
+if x >= 3 and x <= 6 and y >= -5 and y <= 1:
+    print("no yes")
+if x>= -1 and x<=2 and
