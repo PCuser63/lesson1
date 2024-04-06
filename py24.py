08795157d9b77fe5463c2f774fa9308fc28703ad
raiideluxe <artemreshetnikov.one@gmail.com>
2024-04-06 09:47:25 +0400

    Update py24.py

--- py24.py
+++ py24.py
@@ -1,7 +1,14 @@
-def volume(*nums):
-    mul = 1
-    for n in nums:
-        mul*=n
-    print(mul)
-volume(3,2)
-volume(2,2,2)
+import math
+
+def trapez(func, a, b, N):
+    h = (b - a) / N  
+    result = 0.5 * (func(a) + func(b))  
+    
+    for i in range(1, N):
+        result += func(a + i * h)  
+    
+    result *= h  
+    return round(result, 8)  
+
+result = trapez(math.sin, 5, 7, 100)
+print(result)
